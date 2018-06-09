475db439ba912406041919ba4dd87f7ba285bdd0
kitos <kitos78@gmail.com>
2018-06-09 20:15:49 +0200

    alta ConsoleApp4

---
+++ ConsoleApp4/Conversor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Conversor
+    {
+        public int conTryCatch(string entrada)
+        {
+            try
+            {
+                int value = int.Parse(entrada);
+                return value;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public int conTryParse(string entrada)
+        {
+            int value = 0;
+            int.TryParse(entrada, out value);
+            return value;
+        }
+    }
+
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ciclos = 3000;
+            string entrada = "esto debería ser un número";
+            int salida = 0;
+            Conversor conversor = new Conversor();            
+
+            Console.WriteLine("Tiempo 1: " + DateTime.Now.ToString());
+            for (int i = 0; i < ciclos; i++)
+            {
+                salida = conversor.conTryCatch(entrada);
+            }
+
+            Console.WriteLine("Tiempo 2: " + DateTime.Now.ToString());
+
+            for (int i = 0; i < ciclos; i++)
+            {
+                salida = conversor.conTryParse(entrada);
+            }
+
+            Console.WriteLine("Tiempo 3: " + DateTime.Now.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
